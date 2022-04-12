089abe9b26a8a2d2ccaf06bcaf5155dea0c9719f
Teerawat Techa <92813025+AccellutionEvol@users.noreply.github.com>
2022-04-12 11:24:42 +0700

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,23 +1,23 @@
-// plaintext = "Engineering";
-// key = 4;
-// ciphertext = "";
-// for(line=0; line<key-1; line++){
-//     skip=2*(key-line-1);   j=0;
-//     // console.log(skip)
-//      for(i=line; i<plaintext.length;){
-//         // console.log(i);
-//          ciphertext += plaintext.charAt(i);
-//         //  console.log(ciphertext);
-//          if((line==0) || (j%2 == 0)){
-//              i+=skip;
-//             // console.log(i);
-//         } 
-//         else i+=2*(key-1) - skip;  
-//         j++;          
-//      }
-//  }
-//  for(i=line; i<plaintext.length; i+=2*(key-1)) ciphertext += plaintext.charAt(i);
-//  console.log(ciphertext);
+plaintext = "Engineering";
+key = 4;
+ciphertext = "";
+for(line=0; line<key-1; line++){
+    skip=2*(key-line-1);   j=0;
+    // console.log(skip)
+     for(i=line; i<plaintext.length;){
+        // console.log(i);
+         ciphertext += plaintext.charAt(i);
+        //  console.log(ciphertext);
+         if((line==0) || (j%2 == 0)){
+             i+=skip;
+            // console.log(i);
+        } 
+        else i+=2*(key-1) - skip;  
+        j++;          
+     }
+ }
+ for(i=line; i<plaintext.length; i+=2*(key-1)) ciphertext += plaintext.charAt(i);
+ console.log(ciphertext);
 
 ciphertext = "Eenergnigin";
 key = 4;
@@ -33,9 +33,5 @@ for (line = 0; line < key - 1; line++) {
     }
 }
 for (i = line; i < ciphertext.length; i += 2 * (key - 1)) pt[i] = ciphertext.charAt(k++);
-pt.join("");
-for (i = pt.length; i < pt.length; i++) {
-    pt[i] += pt[i + 1]
-    console.log(pt);
-}
-console.log(pt);
+const decoded = pt.join('');
+console.log(decoded);
