49488c77422489b5f5f824826be1abeb1ae6da09
Pathompong ThonChinda <niceooza@gmail.com>
2022-04-21 23:58:24 +0700

    update RSa

--- src/Component/RSA/Rsa.js
+++ src/Component/RSA/Rsa.js
@@ -1,85 +1,154 @@
 import { useState } from "react";
+import bigInt from "big-integer";
 
-function Columnar() {
+function Rsa() {
   const [cText, setCtext] = useState("");
-  const [mTextInput, setMTextInput] = useState(0);
-  const [pTextInput, setPInput] = useState(2);
-  const [qTextInput, setQInput] = useState(3);
+  const [decryptText, setDecryptTexttext] = useState("");
+  const [publicText, setPublictext] = useState("");
+  const [privateText, setPrivatetext] = useState("");
+  const [mTextInput, setMTextInput] = useState("");
+  const [mDTextInput, setMDTextInput] = useState("");
+  const [keyEncryptText, setkeyEncryptText] = useState("");
+  const [keyDecryptText, setkeyDecryptText] = useState("");
 
-  function onEncodeClick() {
-    let n = pTextInput * qTextInput;
-    let phiN = (pTextInput - 1) * (qTextInput - 1);
+  function onDecryptClick(plainText = "") {
+    let pubText = atob(keyDecryptText).split(",");
+    let d = bigInt(pubText[0]);
+    let n = bigInt(pubText[1]);
+    let c = atob(plainText);
+    let m = bigInt(c).modPow(d, n).toString();
+    let asciiText = "";
+    let arr = [];
+    for (let i = 0; i <= m.length; i++) {
+      if (+asciiText >= 32) {
+        arr.push(String.fromCharCode(+asciiText));
+        asciiText = m[i]
+      } else {
+        asciiText += m[i];
+      }
+    }
+    setDecryptTexttext(arr.join(""))
+  }
+
+  function onEnCrpytClick(planText = "") {
+    let pubText = atob(keyEncryptText).split(",");
+    let e = bigInt(pubText[0]);
+    let n = bigInt(pubText[1]);
+    let mtext = [];
+    for (let index = 0; index < planText.length; index++) {
+      let asciiText = planText.charCodeAt(index);
+      mtext.push(asciiText);
+    }
+    let m = bigInt(mtext.join(""));
+    setCtext(btoa(bigInt(m).modPow(e, n).toString()));
+  }
+
+  function onGenerateClick() {
+    let p = rndPrimeNumber("P");
+    let q = rndPrimeNumber("Q", p);
+    let n = bigInt(p).multiply(q);
+    let phiN = bigInt(bigInt(p).minus(1)).multiply(bigInt(q).minus(1));
     let e = rndPrimeNumber("E");
-    let d = 1;
-    if (e < phiN) {
-      setCtext(e);
+
+    if (e < phiN && +bigInt.gcd(e, phiN) === 1) {
+      let d = bigInt(e).modInv(phiN);
+      setPublictext(btoa([e, n]));
+      setPrivatetext(btoa([d, n]));
     } else {
-      rndPrimeNumber("E");
+      return onGenerateClick();
     }
   }
 
-  function rndPrimeNumber(type = "") {
-    let number = Math.floor(Math.random() * 100) + 2;
-    let isPrime = true;
-    for (let i = 2; i < number; i++) {
-      if (number % i === 0) {
-        isPrime = false;
-        break;
+  function rndPrimeNumber(type = "", p = 0) {
+    const min = bigInt.one.shiftLeft(512 - 1);
+    const max = bigInt.one.shiftLeft(512).prev();
+    while (true) {
+      let number = bigInt.randBetween(min, max);
+      if (number.isProbablePrime(256)) {
+        if (type === "P") {
+          return number;
+        } else if (type === "Q") {
+          if (number !== p && Math.abs(p - number) !== 1) {
+            return number;
+          } else {
+            return rndPrimeNumber("Q");
+          }
+        } else if (type === "E") {
+          return number;
+        }
+      } else {
+        return rndPrimeNumber(type);
       }
-    }
-    if (isPrime) {
-      if (type === "P") {
-        setPInput(number);
-      } else if (type === "Q") {
-        setQInput(number);
-      } else if (type === "E") {
-        return number;
-      }
-    } else {
-      return rndPrimeNumber(type);
     }
   }
 
   return (
     <div>
-      <h1>M</h1>
-      <input
-        type="text"
-        placeholder="Plantext"
-        value={mTextInput}
-        onInput={(event) => setMTextInput(event.target.value)}
-      />
-      <h1>P</h1>
-      <input type="number" placeholder="Key" value={pTextInput} />
-      <button
-        onClick={() => {
-          rndPrimeNumber("P");
-        }}
-      >
-        Random
-      </button>
-      <h1>Q</h1>
-      <input type="number" placeholder="Key" value={qTextInput} />
-      <button
-        onClick={() => {
-          rndPrimeNumber("Q");
-        }}
-      >
-        Random
-      </button>
       <div>
         <button
           onClick={() => {
-            onEncodeClick();
+            onGenerateClick();
           }}
         >
-          Encode
+          Generate Key
         </button>
-        <button>Decode</button>
-        <h1>{cText}</h1>
+      </div>
+      <h1>Public Key</h1>
+      <textarea readOnly value={publicText} />
+      <h1>Private Key</h1>
+      <textarea readOnly value={privateText} />
+      <div>
+        <h1>PlainText</h1>
+        <input
+          type="text"
+          placeholder="PlainText"
+          value={mTextInput}
+          onInput={(event) => setMTextInput(event.target.value)}
+        />
+        <h1>Public/Private Key</h1>
+        <div>
+          <textarea
+            value={keyEncryptText}
+            onInput={(event) => setkeyEncryptText(event.target.value)}
+          />
+        </div>
+
+        <button
+          onClick={() => {
+            onEnCrpytClick(mTextInput);
+          }}
+        >
+          Encrypt
+        </button>
+      </div>
+      <h1>{cText}</h1>
+      <div>
+        <h1>PlainText</h1>
+        <input
+          type="text"
+          placeholder="PlainText"
+          value={mDTextInput}
+          onInput={(event) => setMDTextInput(event.target.value)}
+        />
+        <h1>Public/Private Key</h1>
+        <div>
+          <textarea
+            value={keyDecryptText}
+            onInput={(event) => setkeyDecryptText(event.target.value)}
+          />
+        </div>
+
+        <button
+          onClick={() => {
+            onDecryptClick(mDTextInput);
+          }}
+        >
+          Decrypt
+        </button>
+        <h1>{decryptText}</h1>
       </div>
     </div>
   );
 }
 
-export default Columnar;
+export default Rsa;
